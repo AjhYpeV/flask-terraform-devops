52f087c0c5e86bbcdc010f4d6b8072b833a184cc
AjhYpeV <matalowski@ajserver>
2025-07-22 16:14:19 +0200

    Complete DevOps setup with CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,13 +21,22 @@ resource "aws_security_group" "flask_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
+
 
 resource "aws_instance" "flask_app" {
   ami           = "ami-0437df53acb2bbbfd"
--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-yum update -y
-amazon-linux-extras install docker -y
-service docker start
-usermod -aG docker ec2-user
-systemctl enable docker
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -aG docker ec2-user
+sudo systemctl enable docker
 
 # Wait for docker to be ready
 sleep 30
 
 # Pull and run the container
-docker run -d -p 80:5000 --name flask-app --restart unless-stopped ghcr.io/ajhypev/flask-app:latest
+sudo docker run -d -p 80:5000 --name flask-app --restart unless-stopped ghcr.io/ajhypev/flask-terraform-devops/flask-app:latest
